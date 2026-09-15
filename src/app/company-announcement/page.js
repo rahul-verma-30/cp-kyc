be2d43ad4af261504073e6002c0db3e94acbce45
aman singh <2110aman@gmail.com>
2026-09-15 12:17:46 +0530

    feat: add company announcement page, component, and navigation tab

---
+++ src/app/company-announcement/page.js
@@ -0,0 +1,9 @@
+import CompanyAnnouncement from "@/components/CompanyAnnouncement/CompanyAnnouncement";
+
+export default function CompanyAnnouncementPage() {
+  return (
+    <main>
+      <CompanyAnnouncement />
+    </main>
+  );
+}
